72b7c8df17be4639cf7c0cebaa1cf5ba0ee3b7e6
Md Mostafa <mdmostafahakim@gmail.com>
2025-09-19 11:50:08 +0600

    new data

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "doyhiacif",
-  api_key: "221424586279484",
-  api_secret: "7zGbqoNSC4FkgeKWPQcwBPVxTCs",
+  cloud_name: "dxhftyuuo",
+  api_key: "172368462537811",
+  api_secret: "Qm1m0y4lgd2pXdAkFd60vJWHeiw",
 });
 module.exports = cloudinary;
